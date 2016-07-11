f60678beffdb9ac3054887680f169a1f1da30107
xenia-cjen <jonah0604@gmail.com>
2016-07-11 09:51:58 +0800

    path-oriented-buggy

--- pkg/interface/lib/dbg/cell.d
+++
@@ -1,1 +0,0 @@
-lib/dbg/cell.o lib/dbg/cell.d : src/cell.cpp src/cell.h src/global.h
--- pkg/interface/lib/dbg/lex_n_yacc/sdf_file.tab.d
+++
@@ -1,2 +0,0 @@
-lib/dbg/lex_n_yacc/sdf_file.tab.o lib/dbg/lex_n_yacc/sdf_file.tab.d : lib/dbg/lex_n_yacc/sdf_file.tab.cpp src/sdf_file.h \
- src/global.h
