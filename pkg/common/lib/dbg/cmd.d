15bceb7226bca167eaab71bfcc84663f45d2dc37
xenia-cjen <jonah0604@gmail.com>
2016-07-23 19:19:15 +0800

    x-path-support

--- pkg/common/lib/dbg/cmd.d
+++
@@ -1,1 +0,0 @@
-lib/dbg/cmd.o lib/dbg/cmd.d : src/cmd.cpp src/ascii.h src/vt100.h src/cmd.h src/get_opt.h
--- pkg/interface/lib/dbg/lht_pat_file.d
+++
@@ -1,2 +0,0 @@
-lib/dbg/lht_pat_file.o lib/dbg/lht_pat_file.d : src/lht_pat_file.cpp src/lht_pat_file.h src/pat_file.h \
- src/global.h
